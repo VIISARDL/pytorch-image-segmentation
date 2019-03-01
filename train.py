1eefc2ad9fb849c90c569ac48e17b8be03f87782
heitorrapela <hrm@cin.ufpe.br>
2019-03-01 16:01:02 -0300

    Add some modifications (Partially Working)

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def argParser():
 	parser.add_argument("--val_images", type = str , default="dataset/dataset1/images_prepped_test/")
 	parser.add_argument("--val_annotations", type = str , default="dataset/dataset1/annotations_prepped_test/")
 	parser.add_argument("--epochs", type = int, default=100)
-	parser.add_argument("--batch_size", type = int, default=1)
+	parser.add_argument("--batch_size", type = int, default=4)
 
 	return parser.parse_args()
 
@@ -33,40 +33,33 @@ n_classes = args.n_classes
 input_height = args.input_height
 input_width = args.input_width
 validate = args.validate
-epochs = 30#args.epochs
+epochs = 10#args.epochs
 
 images_path = "dataset/dataset1/images_prepped_train/"
 segs_path = "dataset/dataset1/annotations_prepped_train/"
-batch_size = 4#*4
+batch_size = 18
 n_classes = 10
-#input_width = int(200)
-#input_height = int(200)
+input_height = int(200)
+input_width = int(200)
+
 input_channels = 3
 use_cuda = True
 device = torch.device("cuda" if use_cuda else "cpu")
 
-model = Segnet(input_channels=input_channels,input_width=input_width, input_height=input_height, n_classes=n_classes).to(device)
+model = Unet(input_channels=input_channels,input_width=input_width, input_height=input_height, n_classes=n_classes).to(device)
 
-#weights = [0.21085202, 0.23258652, 0.00982927 , 0.31658215, 0.0448627, 0.09724055, 0.01172954, 0.01126809, 0.05865686, 0.00639231]#[0.13097618, 0.25962482, 0.00564374, 0.28953383, 0.08719237, 0.16336204, 0.00892258, 0.03077999, 0.01745943, 0.00650503]
-#[0.2595, 0.1826, 4.5640, 0.1417, 0.9051, 0.3826, 9.6446, 1.8418, 0.6823, 6.2478, 7.3614, 0]
-#class_weights = torch.FloatTensor(weights).cuda()
-#criterion = nn.CrossEntropyLoss(weight=class_weights)
+weights = [0.21085201899788072, 0.23258652172267635, 0.009829274523160764, 0.316582147542638, 0.04486270372893329, 0.09724054521142396, 0.011729535649409628, 0.011268086461802402, 0.0586568555101423, 0.006392310651932587]
+class_weights = 1/torch.FloatTensor(weights).cuda()
+criterion = nn.CrossEntropyLoss(weight=class_weights)
 
-criterion = nn.CrossEntropyLoss()
-learning_rate = 1e-5
-#optimizer = torch.optim.Adam(model.parameters(),learning_rate)#, lr=learning_rate)
-lr=0.1
-optimizer = torch.optim.SGD(model.parameters(),
-                          lr=lr,
-                          momentum=0.9,
-                            weight_decay=1e-6)
-
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
+#criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
 
 custom_dataset = MyCustomDataset(images_path=images_path, segs_path=segs_path, n_classes=n_classes, input_width=input_width,input_height=input_height)
 custom_dataloader = torch.utils.data.DataLoader(dataset=custom_dataset,
                                                 batch_size=batch_size,
-                                                shuffle=False)
+                                                shuffle=True)
 
 
 # labels 0-9
@@ -123,7 +116,7 @@ custom_dataloader_eval = torch.utils.data.DataLoader(dataset=custom_dataset_eval
 
 use_cuda = False
 device = torch.device("cuda" if use_cuda else "cpu")
-model = Segnet(input_channels=input_channels,input_width=input_width, input_height=input_height, n_classes=n_classes).to(device)
+model = Unet(input_channels=input_channels,input_width=input_width, input_height=input_height, n_classes=n_classes).to(device)
 
 colors = [  ( random.randint(0,255),random.randint(0,255),random.randint(0,255)   ) for _ in range(n_classes)  ]
 
